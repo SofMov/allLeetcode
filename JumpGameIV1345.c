1b2e3ade609445b963a562a671b754e11ac56ea4
vodyanoy <mysofia06@gmail.com>
2026-05-20 13:26:57 +0100

    Could not finish as it covered graphs

---
+++ JumpGameIV1345.c
@@ -0,0 +1,16 @@
+struct Node{
+    struct Node** further_nodes; //array of Node pointers
+    int nodeNum; 
+};
+
+void setup(int* arr, int arrSize);
+
+int minJumps(int* arr, int arrSize) {
+
+}
+
+void setup(int* arr, int arrSize){
+    for(int i = 0; i < arrSize; i++){
+        int next = nextNum(arr, arrSize, )
+    }
+}
